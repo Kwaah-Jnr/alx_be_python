3991bb6a2672d42e04718ee11b3d4e3fa11fa615
Kwaah-jnr <kwaahjnr@gmail.com>
2025-01-16 11:51:49 -0800

    This is the introduction to python with ALX

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,14 @@
+# Prompt the user for financial details
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+# Calculate the monthly savings
+monthly_savings = monthly_income - monthly_expenses
+
+# Calculate the projected annual savings with 5% interest
+annual_savings = monthly_savings * 12
+projected_annual_savings = annual_savings + (annual_savings * 0.05)  # 5% interest on total annual savings
+
+# Display the results
+print(f"Your monthly savings are: {monthly_savings:.2f}")
+print(f"Your projected annual savings after including interest is: {projected_annual_savings:.2f}")
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you?"))
+age_in_future = 27 + age
+
+print(f"In 2050, you will be {age_in_future} years old.")
